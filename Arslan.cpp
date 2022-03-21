beb1a6b0f51d6b16f527014a8b0825c1f32ae3db
Arslan <100716135+Arslan909@users.noreply.github.com>
2022-03-22 04:11:26 +0500

    Create Arslan.cpp

---
+++ Arslan.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+    string username;
+  
+    cout << "Enter your username: ";
+    cin >> username;
+
+    std::cout << "Hello, " << username << std::endl;
+  
+    return 0;
+}
