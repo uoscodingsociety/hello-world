72f4aa5a314f9e20cd43447b055ed6eed4a832af
arslan909 <jaranlxix@gmail.com>
2022-03-23 09:51:15 +0500

    used std explicitly

--- Arslan.cpp
+++ Arslan.cpp
@@ -1,11 +1,13 @@
-#include<iostream>
+#include <iostream>
 
-int main(int argc, char const* argv[]) {
-        
+int main(int argc, char const *argv[])
+{
     std::string username;
+
     std::cout << "Enter your username: ";
     std::cin >> username;
 
-    std::cout << "Hello, " << username << "!" << std::endl;
+    std::cout << "Hello, " << username << std::endl;
+
     return 0;
 }
