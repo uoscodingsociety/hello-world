a6e2bf62ddc6bd5c4f9e5f4e5e2dda2877e47bc6
Ahlyab <44720995+Ahlyab@users.noreply.github.com>
2022-03-27 01:29:58 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Made By Ahlyab
+
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string name;
+  
+  std::cout << "Enter Your name : ";
+  std::getline(std::cin, name);
+  
+  std::cout << "Hell0, " << name << std::endl;
+  
+  return 0;
+}
